a1b4b544887996d2cb5a42871dbeb71e58995f61
shaikFZl <shaikfazilshaik80966@gmail.com>
2025-02-09 12:04:54 +0000

    basics of nums and math methods

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,22 @@
+const score = 200
+console.log(score);
+
+const accountBalance = new Number(100);
+console.log(accountBalance.toString());
+console.log(accountBalance.toString().length);
+console.log(accountBalance.toFixed(2));
+
+const anotherNumber = 148.8976;
+console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++ MATHS ++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(8.3));
+console.log(Math.ceil(8.3));
+console.log(Math.floor(8.9));
