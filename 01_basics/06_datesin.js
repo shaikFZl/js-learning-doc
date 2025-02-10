ed0bc12db8dd8d7cfc99f118248e4f8ea220d471
shaikFZl <shaikfazilshaik80966@gmail.com>
2025-02-10 05:26:16 +0000

    basic concepts of date and time in js.

---
+++ 01_basics/06_datesin.js
@@ -0,0 +1,26 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleString);
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toLocaleDateString)
+
+let myTimeStamp = Date.now()
+
+console,log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default', {
+     weekday:"long"
+
+
+}
+)
